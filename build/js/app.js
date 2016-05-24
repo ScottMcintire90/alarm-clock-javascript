06e1c9f74e4092091767446254ba5e050027e053
kendra dunham and scott mcintire <scottmcintire90@gmail.com>
2016-05-24 15:28:49 -0700

    add simple stopwatch

--- build/js/app.js
+++ build/js/app.js
@@ -14,25 +14,50 @@ var update = function() {
   $('#currentTime').text(moment().format('LTS'));
 };
 
+var counter = 0;
+
+var watch = setInterval(function(){ startWatch() }, 1000);
+
+function startWatch() {
+  counter = counter + 1;
+  $('#stopwatchValue').text(counter);
+}
+
+function resetTheWatch() {
+  counter = 0;
+}
+
 $(document).ready(function() {
   setInterval(update, 1000);
+
   $('#alarm').submit(function(event) {
     event.preventDefault();
-
     var time = $('#timepicker5').val();
     var newAlarm = new Alarm(time);
 
     setInterval(function() {
-      $('#hidden-time').text(moment().format('LT'));
       var hidden = moment().format('LT');
       var userAlarm = newAlarm.read();
       if(hidden === userAlarm) {
-        console.log("success!");
+        window.location.href = 'rickroll.html';
       }
     }, 1000);
-
     $('.hide-alarm').show();
     $('#new-alarm').text(newAlarm.read());
+  });
+
+  $('#startWatch').submit(function(event) {
+    event.preventDefault();
+    counter = 0;
+    $('#zero').hide();
+    $('#stopwatchValue').show();
+  });
+
+  $('#resetWatch').submit(function(event) {
+    $('#zero').show()
+    counter = 0;
+    $('#stopwatchValue').hide();
+    event.preventDefault();
   });
 
   $('#timepicker5').timepicker({
